22bf20760eb8d30c50a0615febc0511f2c5077c6
yjleeinkr <lucy.lee930216@gmail.com>
2022-11-09 15:37:18 +0900

    separate api request logic

---
+++ vue-til/src/api/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+// axios 공통 설정 (config)를 create의 인자로 넣어준다.
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+});
+
+function registerUser(userData) {
+  // const url = 'http://localhost:3000/signup';
+  // return axios.post(url, userData);
+  return instance.post('signup', userData);
+}
+
+function loginUser(userData) {
+  return instance.post('login', userData);
+}
+
+export { registerUser, loginUser };
