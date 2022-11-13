57f2bc515641fd7cad060aa8bfbe40cb4e7368e3
yjleeinkr <lucy.lee930216@gmail.com>
2022-11-14 01:37:41 +0900

    separate component for post items

---
+++ vue-til/src/components/posts/PostListItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <div class="post-title">
+      {{ postItem.title }}
+    </div>
+    <div class="post-contents">
+      {{ postItem.contents }}
+    </div>
+    <div class="post-time">
+      {{ postItem.createdAt }}
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    postItem: {
+      type: Object,
+      required: true, // props 가 넘어오지 않았을 경우 error가 뜰거다.
+    },
+  },
+};
+</script>
+
+<style></style>
--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -3,17 +3,23 @@
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
       <ul>
-        <li v-for="postItem in postItems" :key="postItem._id">
-          {{ postItem.title }}
-        </li>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        ></PostListItem>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import PostListItem from '@/components/posts/PostListItem.vue';
 import { fetchPosts } from '@/api/index.js';
 export default {
+  components: {
+    PostListItem,
+  },
   data() {
     return {
       postItems: [],
