2d956a71d881ab9d2c59e2b12681cd70e8ac2be6
yjleeinkr <lucy.lee930216@gmail.com>
2022-11-14 23:17:34 +0900

    make form, page for adding post;

---
+++ vue-til/src/components/posts/PostAddForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="contents">
+    <h1 class="page-header">Create Post</h1>
+    <div class="form-wrapper">
+      <form class="form" @submit.prevent="submitForm">
+        <div>
+          <label for="title">Title:</label>
+          <input id="title" type="text" v-model="title" />
+        </div>
+        <div>
+          <label for="contents">Contents:</label>
+          <textarea id="contents" type="text" rows="5" v-model="contents" />
+        </div>
+        <button type="submit" class="btn">Create</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      contents: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log('hihi ');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-wrapper .form {
+  width: 100%;
+}
+
+.btn {
+  color: white;
+}
+</style>
