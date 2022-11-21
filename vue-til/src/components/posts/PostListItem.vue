37d439b1f8ddd833e227ff7c12736781c307624a
yjleeinkr <lucy.lee930216@gmail.com>
2022-11-22 00:27:35 +0900

    feat : delete function on posts

--- vue-til/src/components/posts/PostListItem.vue
+++ vue-til/src/components/posts/PostListItem.vue
@@ -8,16 +8,27 @@
     </div>
     <div class="post-time">
       {{ postItem.createdAt }}
+      <i class="icon ion-md-create"></i>
+      <i class="icon ion-md-trash" @click="deleteItem"></i>
     </div>
   </li>
 </template>
 
 <script>
+import { deletePost } from '@/api/posts.js';
 export default {
   props: {
     postItem: {
       type: Object,
       required: true, // props 가 넘어오지 않았을 경우 error가 뜰거다.
+    },
+  },
+  methods: {
+    async deleteItem() {
+      if (confirm('You want to delete it?')) {
+        await deletePost(this.postItem._id);
+        this.$emit('refresh');
+      }
     },
   },
 };
